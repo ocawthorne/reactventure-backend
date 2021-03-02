a92d2c66324903e85243429b8152b609c26e6e52
ocawthorne <ocawthorne@gmail.com>
2021-03-02 18:05:07 +0000

    Correct password migration

--- db/migrate/20210218162910_create_users.rb
+++ db/migrate/20210218162910_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username
       t.string :hero_name
-      t.password_digest :password
+      t.string :password_digest
       t.timestamps
     end
   end
