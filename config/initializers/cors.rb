d77b750f029ec405620e20debe52283021cffc30
ocawthorne <ocawthorne@gmail.com>
2021-03-03 18:31:30 +0000

    Change CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -37,9 +37,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
 end
